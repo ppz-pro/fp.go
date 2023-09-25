6a5433384c0f77d822cbc9952f1089a35e340a25
weixianfa <wxf_tensor@163.com>
2023-09-25 12:29:01 +0800

    feat: Filter

--- slice/index.go
+++ slice/index.go
@@ -8,18 +8,36 @@ func New[El any](el []El) Slice[El] {
 	return Slice[El]{el}
 }
 
-type Item[El any] struct {
-	el    El
-	index int
-	slice Slice[El]
+func (slice Slice[El]) Get() []El {
+	return slice.value
 }
 
-func make_container[item any](source []item) []item {
-	return make([]item, 0, len(source))
+func (slice Slice[El]) Len() int {
+	return len(slice.value)
+}
+
+type Item[El any] struct {
+	El    El
+	Index int
+	Slice Slice[El]
+}
+
+func make_container[item any](size int) []item {
+	return make([]item, 0, size)
 }
 
 func (slice Slice[El]) Each(cb func(item Item[El])) {
 	for index, item := range slice.value {
 		cb(Item[El]{item, index, slice})
 	}
+}
+
+func (slice Slice[El]) Filter(cb func(item Item[El]) bool) Slice[El] {
+	var result []El
+	for index, item := range slice.value {
+		if cb(Item[El]{item, index, slice}) {
+			result = append(result, item)
+		}
+	}
+	return New[El](result)
 }
--- slice/index_test.go
+++ slice/index_test.go
@@ -1,6 +1,8 @@
 package slice
 
-import "testing"
+import (
+	"testing"
+)
 
 func is_same_slice[El comparable](a []El, b []El) bool {
 	for i, el := range a {
@@ -11,21 +13,37 @@ func is_same_slice[El comparable](a []El, b []El) bool {
 	return true
 }
 
-func Test_foreach(t *testing.T) {
+func Test_each(t *testing.T) {
 	raw := []int{1, 2, 3}
 
-	test := make_container[int](raw)
+	test := make_container[int](len(raw))
 	count := 0
 
 	New(raw).Each(func(item Item[int]) {
-		if item.index != count {
-			t.Error("index error", item.index, count)
+		if item.Index != count {
+			t.Error("index error", item.Index, count)
 		}
-		test = append(test, item.el)
+		test = append(test, item.El)
 		count = count + 1
 	})
 
 	if !is_same_slice(raw, test) {
 		t.Error("wrong el after each")
+	}
+}
+
+func Test_filter(t *testing.T) {
+	raw := []int{1, 2, 3, 4, 5, 6, 7}
+	odds := New[int](raw).Filter(
+		func(item Item[int]) bool {
+			return item.El%2 == 1
+		},
+	).Get()
+
+	if len(odds) != 4 {
+		t.Error("filter error, length", len(odds))
+	}
+	if !is_same_slice[int](odds, []int{1, 3, 5, 7}) {
+		t.Error("filter error, elements", odds)
 	}
 }
