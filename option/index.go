3b313e0c86ef08ca471d53eced90a9264dcf4fa2
gy <372301467@qq.com>
2023-09-30 11:43:54 +0800

    refactor: option.Match

--- option/index.go
+++ option/index.go
@@ -13,10 +13,22 @@ func Nil[V any]() Option[V] {
 	return Option[V]{has_value: false}
 }
 
-func (opt Option[V]) Match(has_value func(V), no_value func()) {
+func (opt Option[V]) Match(no_value func(), has_value func(V)) {
 	if opt.has_value {
 		has_value(opt.value)
 	} else {
 		no_value()
+	}
+}
+
+func Match[V any, Result any](
+	opt Option[V],
+	no_value func() Result,
+	has_value func(V) Result,
+) Result {
+	if opt.has_value {
+		return has_value(opt.value)
+	} else {
+		return no_value()
 	}
 }
--- option/index_test.go
+++ option/index_test.go
@@ -8,14 +8,14 @@ func Test_option_with_value(t *testing.T) {
 	has_executed := false
 
 	option.Match(
+		func() {
+			t.Error("no_value method should not be executed on option without value")
+		},
 		func(v int) {
 			has_executed = true
 			if v != 1 {
 				t.Error("option value error ", v)
 			}
-		},
-		func() {
-			t.Error("no_value method should not be executed on option without value")
 		},
 	)
 
@@ -30,11 +30,11 @@ func Test_option_without_value(t *testing.T) {
 	has_executed := false
 
 	no_value.Match(
-		func(_ int) {
-			t.Error("has_value shouldn't be executed on option without value")
-		},
 		func() {
 			has_executed = true
+		},
+		func(_ int) {
+			t.Error("has_value shouldn't be executed on option without value")
 		},
 	)
 
