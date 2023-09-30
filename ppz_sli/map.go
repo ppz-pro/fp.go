98d1d4e2d41fbbcf5133fb7aeee8e060e56dc375
gy <372301467@qq.com>
2023-09-30 15:28:42 +0800

    refactor: ppz_sli.Map

--- ppz_sli/map.go
+++ ppz_sli/map.go
@@ -1,6 +1,6 @@
 package ppz_sli
 
-func Map3[Input any, Output any](inputs []Input, cb func(Input, int, []Input) Output) Slice[Output] {
+func Map3[Input any, Output any](inputs []Input, cb func(Input, int, []Input) Output) []Output {
 	result := make_container[Output](len(inputs))
 	for index, item := range inputs {
 		result[index] = cb(item, index, inputs)
@@ -8,13 +8,13 @@ func Map3[Input any, Output any](inputs []Input, cb func(Input, int, []Input) Ou
 	return result
 }
 
-func Map2[Input any, Output any](inputs []Input, cb func(Input, int) Output) Slice[Output] {
+func Map2[Input any, Output any](inputs []Input, cb func(Input, int) Output) []Output {
 	return Map3(inputs, func(el Input, index int, _ []Input) Output {
 		return cb(el, index)
 	})
 }
 
-func Map[Input any, Output any](inputs []Input, cb func(Input) Output) Slice[Output] {
+func Map[Input any, Output any](inputs []Input, cb func(Input) Output) []Output {
 	return Map3(inputs, func(el Input, _ int, _ []Input) Output {
 		return cb(el)
 	})
